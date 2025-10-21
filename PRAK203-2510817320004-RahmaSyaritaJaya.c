9ce0183ff2ae7b1fe0264635ac935345dae2db38
jyaritta <rahmasyaritazaya@gmail.com>
2025-10-21 16:25:04 +0800

    Upload C File

--- PRAK203-2510817320004-RahmaSyaritaJaya.c
+++ PRAK203-2510817320004-RahmaSyaritaJaya.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    float a, b, i, j, x, y, hasil;
+    float a, b, i, j, x, y;
     scanf("%f %f %f %f %f %f", &a, &b, &i, &j, &x, &y);
-    hasil = (a-b)*(i/j)-(x+y);
-    printf("%.3f\n", hasil);
+    printf("%.3f\n", (a-b)*(i/j)-(x+y));
 
     scanf("%f %f", &a, &b);
     scanf("%f %f", &i, &j);
     scanf("%f %f", &x, &y);
-    hasil = (a-b)*(i/j)-(x+y);
-    printf("%.3f\n", hasil);
+    printf("%.3f\n", (a-b)*(i/j)-(x+y));
     return 0;
 }
