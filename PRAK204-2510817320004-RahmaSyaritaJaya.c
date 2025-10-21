cd6334dd3a31d586771242aca98607b245dad800
jyaritta <rahmasyaritazaya@gmail.com>
2025-10-21 16:47:30 +0800

    Upload C File

--- PRAK204-2510817320004-RahmaSyaritaJaya.c
+++ PRAK204-2510817320004-RahmaSyaritaJaya.c
@@ -2,7 +2,7 @@
 #define M_PI 3.14286
 
 int main(){
-    float r, t, hasil;
+    float r, t;
     scanf("%f", &r);
     scanf("%f", &t);
     printf("Volume = %.2f\nLuas = %.2f\nKeliling = %.2f\n", M_PI*r*r*t, (2*M_PI*r)*(r+t), 2*M_PI*r);
@@ -10,4 +10,5 @@ int main(){
     scanf("%f %f", &r, &t);
     printf("Volume = %.2f\nLuas = %.2f\nKeliling = %.2f\n", M_PI*r*r*t, (2*M_PI*r)*(r+t), 2*M_PI*r);
     return 0;
+
 }
